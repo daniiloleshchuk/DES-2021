2a3a9c2bdf91a029313f02c9b9c4a6d467d2a17b
Daniil Oleshchuk <danieloleshuk@gmail.com>
2021-02-04 00:30:58 +0200

    changed routing

--- algo/urls.py
+++ algo/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from algo import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('predict', views.get_prediction),
+    # path('', TemplateView.as_view(template_name='index.html')),
 ]
